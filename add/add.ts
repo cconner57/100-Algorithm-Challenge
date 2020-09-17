48fb10935a4ff0350a2c0b84778ebcdfb984f1e2
Chris Conner <chris.conner57@gmail.com>
2020-09-17 13:47:50 -0700

    Finished more algorithms

--- add/add.ts
+++ add/add.ts
@@ -1,13 +1,19 @@
 function add(param1: number, param2: number): number {
-
+	let total = param1 + param2;
+	return total;
 }
 
-// function add2(param1: number[]): number {
+function add2(...param3: number[]): number {
+	let total = 0;
+    param3.forEach((num) => {
+        total += num
+    })
 
-// }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
+	return total;
+}
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+console.log(add(1, 2));
+console.log(add(3, 2));
 
+console.log(add2(1,2,3,4,5));
+console.log(add2(2,3));
--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,12 @@
 function adjacentElementsProduct(inputArray: number[]): number {
+    let largestProduct = inputArray[0] * inputArray[1]
 
+    for(let n of inputArray){
+        const product = inputArray[n] * inputArray[n + 1]
+        largestProduct = largestProduct < product ? product : largestProduct
+    }
+
+    return largestProduct
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
